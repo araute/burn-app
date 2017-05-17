d8f2bf45dc8a63692f42ff5abdbb3df5a7acedff
Akshat Rautela <rautela.akshat@gmail.com>
2017-05-16 21:19:16 -0700

    check in www

---
+++ www/build/2.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([2],{
+
+/***/ 268:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(18);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__patient_photos__ = __webpack_require__(196);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PatientPhotosPageModule", function() { return PatientPhotosPageModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var PatientPhotosPageModule = (function () {
+    function PatientPhotosPageModule() {
+    }
+    return PatientPhotosPageModule;
+}());
+PatientPhotosPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__patient_photos__["a" /* PatientPhotosPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__patient_photos__["a" /* PatientPhotosPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__patient_photos__["a" /* PatientPhotosPage */]
+        ]
+    })
+], PatientPhotosPageModule);
+
+//# sourceMappingURL=patient-photos.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=2.main.js.map
